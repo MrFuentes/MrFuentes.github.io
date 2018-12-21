06a0878c8c74d283bff2b1c692cdca1fc6a88a68
MrFuentes <seangit99@gmail.com>
2018-12-21 16:03:36 +0000

    script now overwrites old html

--- scripts/webify.sh
+++ scripts/webify.sh
@@ -5,6 +5,6 @@ OUTPUT=$2
 pandoc -o "../html/$OUTPUT.html" $INPUT
 pandoc $INPUT --pdf-engine=xelatex -o "../pdfs/$OUTPUT.pdf"
 python3 update_lists.py $OUTPUT
-cat top.html html_list.html middle.html pdf_list.html bottom.html >> ../index.html
+cat top.html html_list.html middle.html pdf_list.html bottom.html > ../index.html
 
 exit 0
